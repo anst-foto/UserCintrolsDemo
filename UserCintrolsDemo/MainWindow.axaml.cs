264349e04f9a0486d34587fb3ea6806af57cced0
Starinin Andrey <starininandrey@gmail.com>
2023-09-03 07:56:34 +0300

    Update

--- UserCintrolsDemo/MainWindow.axaml.cs
+++ UserCintrolsDemo/MainWindow.axaml.cs
@@ -1,11 +1,24 @@
 using Avalonia.Controls;
+using Avalonia.Interactivity;
 
 namespace UserCintrolsDemo;
 
 public partial class MainWindow : Window
 {
+    private InputControl Input1 { get; set; }
+    private InputControl Input2;
     public MainWindow()
     {
+        Input1 = new InputControl("Name");
+        Input2 = new InputControl("Age");
         InitializeComponent();
+        
+        Main.Children.Add(Input1);
+        Main.Children.Add(Input2);
+    }
+
+    private void Button_OnClick(object? sender, RoutedEventArgs e)
+    {
+        Output.Text = $"{Input1.Text} {Input1.Text}";
     }
 }
