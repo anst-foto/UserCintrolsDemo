cd62ff07dc5be18aa6097b523a6d1c6a8b3b1de9
Starinin Andrey <starininandrey@gmail.com>
2023-09-03 07:56:19 +0300

    Initial commit

---
+++ UserCintrolsDemo/InputControl.axaml.cs
@@ -0,0 +1,25 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace UserCintrolsDemo;
+
+public partial class InputControl : UserControl
+{
+    private string _label;
+    
+    public string Text { get; set; }
+    
+    public InputControl(string label)
+    {
+        _label = label;
+        
+        InitializeComponent();
+        LabelInput.Content = _label;
+    }
+
+    private void Input_OnTextChanged(object? sender, TextChangedEventArgs e)
+    {
+        Text = Input.Text;
+    }
+}
